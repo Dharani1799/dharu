2dbcbdfd6e40eb38d7628b402533f4cdd2db6be8
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-11 18:56:13 +0530

    44.c

---
+++ 44.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        if(n==i)
+        c++;
+    }
+        if(c==1)
+    
+        printf("yes");
+        else
+        printf("no");
+
+}
