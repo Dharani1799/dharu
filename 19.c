cba1014bdb87427ce74d00be73ec95a3345da370
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-12 06:35:09 +0530

    19.c

---
+++ 19.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+void main()
+{
+  int c, n, f = 1;
+
+  scanf("%d", &n);
+ 
+  for (c = 1; c <= n; c++)
+    f = f * c;
+ 
+  printf("Factorial of %d = %d\n", n, f);
+ 
+  
+}
